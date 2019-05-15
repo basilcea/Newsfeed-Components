95ba9bc5a0781aeed4522368645b231d950e6283
cea <ogbonnabasil@Ogbonnas-MacBook-Pro.local>
2019-05-15 18:37:00 +0100

    add transition to  menu list

--- Article/Article.js
+++ Article/Article.js
@@ -1,24 +1,38 @@
 // Because classes are not hoisted you will need to start your code at the bottom of the page.  Look for the comment "START HERE"
 
 class Article {
-  constructor(domElement) {
+  constructor(domElement,headingValue , date ,paragraph,) {
     // assign this.domElement to the passed in domElement
     this.domElement = domElement ; 
+    this.headingValue = headingValue;
+    this.date = date;
+    this.paragraph = paragraph;
     // create a reference to the ".expandButton" class. 
     this.expandButton = this.domElement.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expandButton.textContent ='expand';
-     
-    
+  
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandButton.addEventListener('click',()=> this.expandArticle())
-    
+    //this.createArticle();
   }
 
   expandArticle() {
     this.domElement.classList.toggle('article-open');
     // Using our reference to the domElement, toggle a class to expand or hide the article.
   }
+  // createArticle(){
+    // const heading = document.createElement('h2');
+    // heading.innerText = this.heading;
+    // this.domElement.append(heading)
+    // for(let i=0 ;i< 3; i++){
+      // this.domElement.append( document.createElement('p'))
+    // }
+    // const par = this.domElement.querySelectorAll('p');
+  //  par[0].innerText =this.date.bind(this);
+    // par[1].innerText = this.paragraph(this);
+  // }
+ 
 }
 
 /* START HERE: 
--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,14 +1,25 @@
-
-const toggleMenu = (value) => {
-  // Toggle the "menu--open" class on your menu refence.  
-  value.classList.toggle('menu--open')
-}
-
 // Start Here: Create a reference to the ".menu" class
-const menu = document.querySelector('.menu')
+const menu = document.querySelector(".menu");
+
+const toggleMenu = ()=> {
+  // Toggle the "menu--open" class on your menu refence.
+
+  if (menu.classList.contains("menu--open")) {
+    console.log('hi')
+    TweenMax.fromTo('.menu', .8, { x:0,display:'block'},{x:-350})
+  } else {
+    console.log(menu.classList.contains("menu--open"))
+    TweenMax.fromTo('.menu', .8, {x:-100},{x:0})
+    console.log('lo')
+  }
+  menu.classList.toggle("menu--open");
+};
+
+
 // create a reference to the ".menu-button" class
-const menuButton = document.querySelector('.menu-button')
+const menuButton = document.querySelector(".menu-button");
 // Using your menuButton reference, add a click handler that calls toggleMenu
-menuButton.addEventListener('click', ()=>{
-  toggleMenu(menu)
-})
+
+menuButton.addEventListener("click", () => {
+  toggleMenu();
+});
